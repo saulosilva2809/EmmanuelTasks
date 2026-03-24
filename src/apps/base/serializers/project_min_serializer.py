fe5d913ae5b49e9f3aac3bf64b2c26501535ec52
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2026-03-24 08:52:35 -0300

    feat: change the name of the status field to obj_status

--- src/apps/base/serializers/project_min_serializer.py
+++ src/apps/base/serializers/project_min_serializer.py
@@ -7,7 +7,11 @@ from .user_min_serializer import UserMinSerializer
 
 class ProjectMinSerializer(serializers.ModelSerializer):
     owner = UserMinSerializer(read_only=True)
+    project_status = serializers.SerializerMethodField()
 
     class Meta:
         model = ProjectModel
-        fields = ('id', 'name', 'owner', 'status')
+        fields = ('id', 'name', 'owner', 'project_status')
+
+    def get_project_status(self, obj):
+        return obj.get_status_display()
--- src/apps/base/serializers/sprint_min_serializer.py
+++ src/apps/base/serializers/sprint_min_serializer.py
@@ -9,7 +9,11 @@ from .team_min_serializer import TeamMinSerializer
 class SprintMinSerializer(serializers.ModelSerializer):
     project = ProjectMinSerializer(read_only=True)
     team = TeamMinSerializer(read_only=True)
+    sprint_status = serializers.SerializerMethodField()
 
     class Meta:
         model = SprintModel
-        fields = ('id', 'name', 'project', 'team', 'status')
+        fields = ('id', 'name', 'project', 'team', 'sprint_status')
+
+    def get_sprint_status(self, obj):
+        return obj.get_status_display()
