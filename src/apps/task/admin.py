19d3b4113c568e4ed1a2ef8a7a5a4d9f0ada9c96
saulosilva2809 <saulosilva2809@users.noreply.github.com>
2026-03-25 09:46:59 -0300

    feat: include new field in admin

--- src/apps/task/admin.py
+++ src/apps/task/admin.py
@@ -5,8 +5,8 @@ from .models import TaskModel
 
 @admin.register(TaskModel)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'project', 'team', 'status', 'priority', 'responsible', 'due_date']
-    list_filter = ['status', 'priority', 'project', 'team']
-    search_fields = ['title', 'description']
+    list_display = ['id', 'title', 'project', 'team', 'status', 'completed_at', 'priority', 'responsible', 'due_date']
+    list_filter = ['status', 'completed_at', 'priority', 'project', 'team']
+    search_fields = ['title', 'completed_at', 'description']
     list_editable = ['status', 'priority', 'responsible'] # permite editar direto na lista!
     date_hierarchy = 'due_date' # cria um navegador por datas no topo
